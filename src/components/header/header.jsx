21c01700ab4084f94f32193dd0f4bc7e362188fe
mzffrtp <mzffr.tpgl@gmail.com>
2023-09-12 19:10:05 +0200

    header, main page

---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header () {
+    return (
+        <header>
+            <img src="https://pic.onlinewebfonts.com/thumbnails/animations_367347.svg?width=2"/>
+            <h2>Sky Watch Pro</h2>
+        </header>
+    )
+}
---
+++ src/components/map-view/index.jsx
@@ -0,0 +1,5 @@
+export default function MapView () {
+    return (
+        <div>Map View</div>
+    )
+}
