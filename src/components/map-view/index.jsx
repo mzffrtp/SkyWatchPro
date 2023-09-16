aef7423b59827e80488c7866c0355f1cc952f12a
mzffrtp <mzffr.tpgl@gmail.com>
2023-09-16 14:01:47 +0200

    redux store

--- src/components/map-view/index.jsx
+++ src/components/map-view/index.jsx
@@ -1,17 +1,23 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import { useSelector } from 'react-redux';
 
 export default function MapView () {
+    const flightsSlice = useSelector((state)=>state.reducer)
+    console.log(flightsSlice);
     return (
         <div>
             <h3>Map View</h3>
             <div className='map-wrapper'>
-                <MapContainer center={[59.3293, 18.0686]} zoom={13} scrollWheelZoom={true}>
+                <MapContainer center={[59, 15]} zoom={6} scrollWheelZoom={true}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[59.3293, 18.0686]}>
+                    {
+                        console.table("flights",flightsSlice.flights)
+                    }
+                    <Marker position={[59, 15]}>
                     <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
--- src/redux/flightSlice.js
+++ src/redux/flightSlice.js
@@ -11,8 +11,15 @@ const initialState = {
 
 
 export const getFlightData = createAsyncThunk("flights/getflights", async () => {
-    const flightRes = await axios.request(options);
-    console.log(flightRes);
+    const res = await axios.request(options);
+    const flightRes = res.data.aircraft.map((plane) => ({
+        id: plane[0],
+        code: plane[1],
+        lat: plane[2],
+        lng: plane[3],
+    }))
+
+    return flightRes
 
 });
 
@@ -20,6 +27,19 @@ export const flightSlice = createSlice({
     name: "flightSlice",
     initialState,
     extraReducers: {
+        [getFlightData.pending]: (state, action) => {
+            state.flightsLoading = true
+            console.log("flightsLoading", flightsLoading);
+        },
+        [getFlightData.fulfilled]: (state, action) => {
+            state.flights = action.payload;
+            state.flightsLoading = false
+        },
+        [getFlightData.rejected]: (state, action) => {
+            state.isError = true;
+            state.flightsLoading = false;
+            console.log("data fetch error", state.isError);
+        },
 
     }
 })
